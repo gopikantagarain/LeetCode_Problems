b1042cae990e2de701638a4092fc881aa2a0ffa5
gopikantagarain <95495030+gopikantagarain@users.noreply.github.com>
2024-05-27 15:53:58 +0530

    Time: 1 ms (86.90%) | Memory: 42.5 MB (94.08%) - LeetSync

--- 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.java
+++ 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.java
@@ -1,17 +1,41 @@
-public class Solution {
-        public List<List<Integer>> levelOrderBottom(TreeNode root) {
-            List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
-            levelMaker(wrapList, root, 0);
-            return wrapList;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result=new ArrayList<>();
+        if(root==null){
+            return result;
         }
-        
-        public void levelMaker(List<List<Integer>> list, TreeNode root, int level) {
-            if(root == null) return;
-            if(level >= list.size()) {
-                list.add(0, new LinkedList<Integer>());
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int levelSize=queue.size();
+            List<Integer> currentLevel=new ArrayList<>(levelSize);
+            for(int i=0;i<levelSize;i++){
+                TreeNode CurrentNode=queue.poll();
+                currentLevel.add(CurrentNode.val);
+                if(CurrentNode.left !=null){
+                    queue.offer(CurrentNode.left);
+                }
+                if(CurrentNode.right !=null){
+                    queue.offer(CurrentNode.right);
+                }
             }
-            levelMaker(list, root.left, level+1);
-            levelMaker(list, root.right, level+1);
-            list.get(list.size()-level-1).add(root.val);
+            result.add(0,currentLevel);
         }
+        return result;
     }
+}
