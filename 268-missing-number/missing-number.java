f073d871758f9ab1855cc3c4495c5a071997325a
gopikantagarain <95495030+gopikantagarain@users.noreply.github.com>
2024-07-18 15:36:47 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (22.32%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,10 +1,23 @@
+// class Solution {
+//     public int missingNumber(int[] nums) {
+//         int result = nums.length;
+//         for(int i=0;i<nums.length;i++) {
+//             result += i - nums[i];
+//         }
+//         return result;
+        
+//     }
+// }
+
 class Solution {
     public int missingNumber(int[] nums) {
-        int result = nums.length;
-        for(int i=0;i<nums.length;i++) {
-            result += i - nums[i];
+        int n = nums.length;
+        int ans = 0;
+        int arraySum = 0;
+        for(int num: nums) {
+            arraySum += num;
         }
-        return result;
-        
+        int sum = (n*(n+1))/2;
+        return ans=(sum-arraySum);
     }
 }
