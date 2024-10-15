c962796f7ceb87a4221648c7664d45fad4e3e293
gopikantagarain <95495030+gopikantagarain@users.noreply.github.com>
2024-10-15 16:33:00 +0530

    Time: 10 ms (68.08%) | Memory: 42.4 MB (55.61%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,20 +1,38 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
 
-        int map1[]=new int[200];
-        int map2[]=new int[200];
+        // int map1[]=new int[200];
+        // int map2[]=new int[200];
 
-        if(s.length()!=t.length())
+        // if(s.length()!=t.length())
+        //     return false;
+
+
+        // for(int i=0;i<s.length();i++)
+        // {
+        //     if(map1[s.charAt(i)]!=map2[t.charAt(i)])
+        //         return false;
+
+        //     map1[s.charAt(i)]=i+1;
+        //     map2[t.charAt(i)]=i+1;
+        // }
+        // return true;
+        HashMap<Character,Character> mp=new HashMap<Character,Character>();
+        if(s.length() != t.length()){
             return false;
-
-
-        for(int i=0;i<s.length();i++)
-        {
-            if(map1[s.charAt(i)]!=map2[t.charAt(i)])
+        }
+        for(int i=0;i<s.length();i++){
+            Character sch=s.charAt(i);
+            Character tch=t.charAt(i);
+            if(mp.containsKey(sch)){
+                if(mp.get(sch) != tch) return false;
+            }
+            else if(mp.containsValue(tch)){
                 return false;
-
-            map1[s.charAt(i)]=i+1;
-            map2[t.charAt(i)]=i+1;
+            }
+            else{
+                mp.put(sch,tch);
+            }
         }
         return true;
     }
